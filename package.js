45527d7bbd5d9378c0bff5d636dad1cf26df19db
haosdent <haosdent@gmail.com>
2015-03-27 08:28:18 +0800

    Update package.js

--- package.js
+++ package.js
@@ -1,24 +1,21 @@
 Package.describe({
 	name: 'rocketchat:irc',
 	version: '0.0.1',
-	summary: '',
+	summary: 'RocketChat libraries',
 	git: ''
-});
-
-Npm.depends({
-	'irc': '0.3.12'
 });
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.0');
 
-	api.use('coffeescript');
+	api.use([
+		'coffeescript',
+		'rocketchat:lib@0.0.1'
+	]);
 
 	api.addFiles('irc.server.coffee', 'server');
 
 	api.export(['Irc'], ['server']);
 });
 
-Package.onTest(function(api) {
-
-});
+Package.onTest(function(api) {});
